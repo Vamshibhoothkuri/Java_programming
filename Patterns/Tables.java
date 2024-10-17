c23c1c6b51a7fae2f692b6d30ff37332af07e4a3
Vamshibhoothkuri <113632072+Vamshibhoothkuri@users.noreply.github.com>
2024-10-17 20:01:40 +0530

    Add files via upload

---
+++ Patterns/Tables.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+
+public class Tables {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(String.format("%2dX%-2d=%-3d",j,i,j*i));
+            }
+            System.out.println();
+        }
+    }
+}
